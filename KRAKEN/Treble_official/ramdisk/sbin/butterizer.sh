34fb036919b0b98aee7bdafa2b9a27b49cffe30b
SPART4NICUS <paradelovhristiyan@gmail.com>
2020-06-26 15:10:47 +0200

    tweak butterizer
    Signed-off-by: SPART4NICUS <paradelovhristiyan@gmail.com>

--- KRAKEN/Treble_official/ramdisk/sbin/butterizer.sh
+++ KRAKEN/Treble_official/ramdisk/sbin/butterizer.sh
@@ -23,6 +23,12 @@ echo "1" > /sys/devices/system/cpu/cpu0/online
 echo "interactives9" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo "343000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo "1690000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo "1550" > /sys/devices/battery/ac_charge 
+echo "Y" > /sys/module/autosmp/parameters/enabled
+echo "4" > /sys/kernel/autosmp/conf/max_cpus_bc
+echo "4" > /sys/kernel/autosmp/conf/max_cpus_lc
+echo "1" > /sys/kernel/autosmp/conf/min_cpus_bc
+echo "1" > /sys/kernel/autosmp/conf/min_cpus_lc
 # Governor values;
 echo "0" > /sys/devices/system/cpu/cpu0/cpufreq/interactives9/above_hispeed_delay
 echo "1" > /sys/devices/system/cpu/cpu0/cpufreq/interactives9/boost
